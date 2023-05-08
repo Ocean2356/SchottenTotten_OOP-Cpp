ea1d24da6236d75da33099e968f63459e5a7dc6c
Ines-abb <128798478+Ines-abb@users.noreply.github.com>
2023-05-08 17:23:37 +0200

    partie.h

--- partie.h
+++ partie.h
@@ -1,29 +1,79 @@
 #ifndef PARTIE_H
 #define PARTIE_H
 
+#include <iostream>
 #include <string>
 #include <array>
 #include <vector>
 
 class Joueur;
 
+
+
 using namespace std;
-using Force = size_t;
-using Ordre = array<Joueur&, 2>;
-using Resultat = array<int, 2>;
-using Movement = string;
+using Force = size_t; //numéro de la carte
+using Ordre = array<Joueur&, 2>; // ordre des joueurs qui jouent
+using Resultat = array<int, 2>; // score des joueurs
+using Movement = string; // "piocher" ou "jouer"
 
 
-enum class Couleur
+
+class PartieException {
+    public:
+        PartieException(const string& i) :info(i) {}
+        string getInfo() const { return info; }
+    private:
+        string info;
+    };
+
+    // caractéristiques
+    enum class Couleur {Rouge, Marron,Jaune,Vert,Bleu,Violet };
+    enum class Force { un=1, deux=2, trois=3, quatre=4, cinq=5, six=6, sept=7, huit=8, neuf=9 };
+    
+
+    // conversion d'une caractéristique en string
+    string toString(Couleur c);
+    string toString(Force f);
+   
+
+    // écriture d'une caractéristique sur un flux ostream
+    ostream& operator<<(ostream& f, Couleur c);
+    ostream& operator<<(ostream& f, Force x);
+
+    // listes contenant les valeurs possibles pour chacune des caractéristiques
+    extern std::initializer_list<Couleur> Couleurs;
+    extern std::initializer_list<Force> Forces;
+
+    // affichage des valeurs possibles pour chaque caractéristiques
+    void printCouleurs(std::ostream& f = cout);
+    void printForces(std::ostream& f = cout);
+
+
+
+template <class Carte, size_t N> 
+class Pioche
 {
-    Rouge,
-    Marron,
-    Jaune,
-    Vert,
-    Bleu,
-    Violet
-};
+public:
+    bool estVide() const { return nbCartes == 0; };
+    size_t getNbCartes() const { return nbCartes; };
+    bool piocher(Carte &carte) {
+        if (estVide())
+            return false;
+        carte = getCarte();
+        return true;
+    };
+    void placerDessous(Carte &carte){
+        cartes[cartes.size()] = carte;
+    };
+    Pioche(): cartes(), nbCartes(N){};
+    Carte &operator[](size_t i) const { return &cartes[i]; };
 
+private:
+    array<Carte *, N> cartes;
+    size_t nbCartes = 0;
+    vector<Carte> cartesDessous;
+    Carte &getCarte();
+};
 
 class Carte
 {
@@ -32,7 +82,10 @@ public:
     string getDescription() const;
 };
 
-class CarteNormale : public Carte
+std::ostream& operator<<(std::ostream& f, const Carte& c);
+
+
+class CarteNormale : public Carte 
 {
 public:
     CarteNormale(Couleur _couleur, Force _force) : couleur(_couleur), force(_force){};
@@ -45,55 +98,29 @@ private:
     Force force;
 };
 
-template <class Carte, size_t N>
-class Pioche
-{
-public:
-    bool estVide() const { return nbCartes == 0; }
-    size_t getNbCartes() const { return nbCartes; }
-    bool piocher(const Carte& carte) {
-        if (estVide())
-            return false;
-        size_t x = rand()%nbCartes;
-        carte = cartes[x]; // creation d'une référence sur la carte piochee
-        cartes[x] = cartes[--nbCartes];  // déplacement de la dernière carte à l'emplacement de la carte pioche
-        // il faut utiliser 'swap', et de plus vérifier le fonctionnement de & : on veut que la contenue réelle de l'objet carte soit piochée, pas la carte qui se trouve dans cartes[x]
-        return true;
-    }
-    Pioche(): cartes(), nbCartes(N){};
-    Carte& operator[](size_t i) const { return &cartes[i]; }
-
-private:
-    array<Carte *, N> cartes;
-    size_t nbCartes = 0;
-};
-
-/*
-class PiochePartieTactique : public Pioche{
-public:
-     void placerDessous(Carte &carte);
-private:
-     vector<Carte> cartesDessous;
-};
-*/
-
-
 class Tuile
 {
 public:
     Tuile(): nb_pleine(0){}
-    bool est_pleine(){
+    bool est_pleine() const{
         if (cartes_posees[0].size() == nb_pleine && cartes_posees[1].size() == nb_pleine)
             return 1;
         return 0;
     }
+    bool estRevendiquée() const {}
 private:
     array<vector<Carte>, 2> cartes_posees;
     unsigned int nb_pleine;
 };
 
 class Frontiere{
-
+    public:
+    bool estFinie() const { 
+        if(tuiles[0].est_pleine() && tuiles[1].est_pleine() && tuiles[2].est_pleine())
+            return 1;
+        return 0;
+     };
+    Joueur gagnant() const{};
 private:
     static const int nb_tuile = 9;
     array<Tuile, nb_tuile> tuiles;
@@ -102,11 +129,11 @@ private:
 class Main
 {
 public:
-    Main(size_t taille):cartes(taille){}
+    Main(size_t taille):cartes(taille){};
     bool estVide() const { return nbCartes == 0; };
     size_t getNbCartes() const { return nbCartes; };
-    Carte &jouerCarte(size_t i);
-    void piocherCarte(Carte &carte);
+    Carte &jouerCarte(size_t i){return *cartes[i];};
+    void piocherCarte(Carte &carte){cartes.push_back(&carte); nbCartes++;};
 
 private:
     vector<Carte*> cartes;
