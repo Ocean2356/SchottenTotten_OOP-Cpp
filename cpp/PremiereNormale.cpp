2ff35003a6c51ecc21ec063c9b7151e83d0be316
Ines-abb <128798478+Ines-abb@users.noreply.github.com>
2023-06-05 22:23:17 +0200

    Debut du .h

--- cpp/PremiereNormale.cpp
+++ cpp/PremiereNormale.cpp
@@ -1,1 +1,64 @@
 #include "../h/partie.h"
+#include <random>
+#include <chrono>
+
+// Fonction permettant de retourner une string correspondant à une couleur donnée
+string toString(Couleur c) {
+    switch (c) {
+        case Couleur::Rouge:
+            return "Ro";
+        case Couleur::Marron:
+            return "Ma";
+        case Couleur::Jaune:
+            return "Ja";
+        case Couleur::Vert:
+            return "Ve";
+        case Couleur::Bleu:
+            return "Bl";
+        case Couleur::Violet:
+            return "Vi";
+        default:
+            throw PartieException("Couleur inconnue");
+    }
+}
+
+// Fonction permettant de retourner une string correspondant à une force donnée
+string toString(Force f) {
+    return to_string(f);
+}
+
+// Méthode permettant d'échanger deux cartes de la pioche
+void Pioche::swapCartes(Carte* c1, Carte* c2){
+    Carte* tmp = c1;
+    c1 = c2;
+    c2 = tmp;
+};
+
+// Méthode permettant de piocher une carte
+bool Pioche::piocher(Carte **carte) {
+    if (estVide())
+        throw PartieException("La pioche est vide");
+    else {
+        // Génération d'un nombre aléatoire entre 0 et nbCartes - 1 (pour piocher une des cartes piochable de la pioche)
+        std::default_random_engine random_eng(std::chrono::system_clock::now().time_since_epoch().count());
+        std::uniform_int_distribution <int> distrib {0, (int) nbCartes-1};
+        size_t x = distrib(random_eng);
+
+        // On "supprime" la carte piochée, en l'échangeant avec la dernière carte de la pioche et en décrementant le nombre de cartes piochable
+        swapCartes(carte[x], cartes[--nbCartes]);
+        return carte[nbCartes];  // on retourne la carte que l'on vient de "supprimer"
+    }
+}
+
+
+void Main::piocherCarte(Carte* c){
+    cartes.push_back(c);
+}
+
+Carte* Main::jouerCarte(size_t i) {
+    Carte* c = cartes[i];
+    cartes.erase(cartes.begin() + i);
+    return c;
+}
+
+
--- h/partie.h
+++ h/partie.h
@@ -1,5 +1,7 @@
 #pragma once
 
+
+#include <iostream>
 #include <string>
 #include <vector>
 #include <array>
@@ -19,11 +21,29 @@ enum class NumJoueur {joueur1, joueur2};
 enum class TuileRevendiquee {non_revendiquee, revendiquee_joueur1, revendiquee_joueur2};
 enum class Combinaison {somme, suite, couleur, brelan, suite_couleur};
 
+// conversion d'une caractéristique en string
+string toString(Couleur c);
+string toString(Force f)
+
+
+class PartieException : public exception
+{
+    public:
+        PartieException(const string& message) : info(message){}
+        ~PartieException() = default;
+        string getInfo() const { return info; }
+        //const char* what() const noexcept override {return info.c_str();}
+    private:
+        string info;
+};
+
+
 class Carte
 {
 	public:
 		virtual string getInfo() const;
 		virtual string getDescription() const;
+
 };
 
 class CarteNormale : public Carte
@@ -31,11 +51,13 @@ class CarteNormale : public Carte
 	public:
 		 CarteNormale(Couleur c, Force f) : couleur(c), force(f){}
 		 ~CarteNormale() = default;
-		 string getInfo() const;
-		 string getDescription() const;
+         string getInfo() const{ return toString(couleur) + toString(force) + ' ';};
+		 string getDescription() const {return description;};
+
 	private:
 		 Couleur couleur;
 		 Force force;
+         string description;
 };
 
 class Pioche
@@ -43,24 +65,26 @@ class Pioche
 	public:
 		Pioche(Carte** _cartes, size_t nbCartes) : cartes(_cartes){};
 		~Pioche() = default;
+        size_t getNbCartes() const {return nbCartes;}; // Méthode retournant le nombre de cartes piochables
 		bool piocher(Carte** carte);
+        void swapCartes(Carte* c1, Carte* c2); // Méthode permettant d'échanger deux cartes de la pioche
 	private:
 		Carte** cartes;
 		size_t nbCartes;
-		bool estVide() const;
-		size_t getNbCartes() const;
+		bool estVide() const{return nbCartes == 0;}; // Méthode indiquant si la pioche est vide
+
 };
 
-class Main
+class Main  // classe permettant de représenter une main
 {
 	public:
 		Main(size_t taille) : taille_max(taille){}
 		~Main() = default;
-		void piocherCarte(Carte* c);
-		Carte* getCarte(size_t i);
+		void piocherCarte(Carte* c); // Méthode permettant de piocher une carte, c'est-à-dire de l'ajouter à la main
+        Carte* jouerCarte(size_t i); // Méthode permettant de supprimer une carte de la main
 	private:
-		size_t taille_max;
-		vector<Carte*> cartes;
+		size_t taille_max; // par défaut, la main contient au maximum 6 cartes. Ce nombre est modifié pour la partie tactique
+        vector<Carte*> cartes;
 };
 
 class Agent
